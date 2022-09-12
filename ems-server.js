7844be141028b241ae86de13eff3bb9c7d02d6a1
bknasam <bknasam@sreebtech.com>
2022-09-12 14:15:14 +0530

    added services in ems_server.js file

--- ems-server.js
+++ ems-server.js
@@ -23,7 +23,67 @@ app.all("*", function (req, res, next) {
 });
 
 
+/** setattendanceapprovalstatus
+ `set_reason_master`(
+	in reason_id int(11),
+	in reason varchar(64),
+    in reason_status int(11), -- values: 1(Active) , 2(Inactive)
+    in actionby int(11)
+) */
+app.post('/api/set_reason_master', function (req, res) {
+    try {
+        con.query("CALL `set_reason_master` (?,?,?,?)",
+        [req.body.reason_id,req.body.reason, parseInt(req.body.reason_status),req.body.actionby], function (err, result, fields) {
+               
+                    if (result[0][0].statuscode == 0) {
+                        res.send({ status: true, message: "",data: result[0][0]})
+                    } else {
+                        res.send({ status: false, message: "unable to save" })
+                    }
+                
+            });
+
+    }catch (e) {
+        console.log('set_reason_master')
+    }
+})
+
+/**Get Reason Data **/
+
+ app.get('/api/getActiveReasonList', function (req, res) {
+    try {
+        con.query("CALL `get_active_reasons` ()", function (err, result, fields) {
+            if (result && result.length > 0) {
+                res.send({ data: result[0], status: true });
+            } else {
+                res.send({ status: false })
+            }
+        });
+    } catch (e) {
+        console.log('get_reason_master :', e)
+
+    }
+});
+
+/**Get reason list
+ **@reason_id  parameters
+ * **/
+
+ app.get('/api/getReasonMasterData/:reasonid', function (req, res) {
+    try {
+        con.query("CALL `get_reason_master` (?)", [req.params.employee_id], function (err, result, fields) {
+            if (result && result.length > 0) {
+                res.send({ data: result[0], status: true });
+            } else {
+                res.send({ status: false })
+            }
+        });
 
 
+    } catch (e) {
+        console.log('get_reason_master :', e)
+
+    }
+});
 
 module.exports = app;
