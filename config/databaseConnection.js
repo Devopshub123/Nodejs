500019d308003d315213bc5a72622c3900ba5c79
rthallapelly <rthallapelly@sreebtech.com>
2023-03-03 18:48:58 +0530

    set_pay_group_to_employee updated procedure integrated

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -5,8 +5,8 @@ function switchDatabase() {
   return mysql.createConnection({
     /** Local */
     
-          // host: "192.168.1.30",
-          host: "122.175.62.210",
+          host: "192.168.1.30",
+          // host: "122.175.62.210",
             user: "spryple_client_user",
             port: 3306,
             password: "Client&*123",
@@ -33,8 +33,8 @@ async function getNewDBConnection(companyName,dbName)
 
   return new Promise((res,rej)=>{
     var connectionParams = {
-      // host: "192.168.1.30",
-      host: "122.175.62.210",
+      host: "192.168.1.30",
+      // host: "122.175.62.210",
       user: "spryple_client_user",
       port: 3306,
       password: "Client&*123",
