579b29496a40a420da3b66e2e39dc06e2b8fe391
bknasam <bknasam@sreebtech.com>
2023-05-03 06:43:05 +0530

    update the code in common.js and script

--- common.js
+++ common.js
@@ -8,7 +8,7 @@ var nodemailer = require('nodemailer')
 var crypto = require("crypto");
 var algorithm = "aes-256-cbc";
 var mysql = require('mysql');
-const  {spawn }= require('child_process');
+const { spawn } = require('child_process');
 // generate 16 bytes of random data
 var initVector = crypto.randomBytes(16);
 var Securitykey = crypto.randomBytes(32);
@@ -2079,8 +2079,8 @@ async function setSprypleClientPlanPayment(req, res) {
             res.send({status:false})
            }
            else {
-            let Payment =  await paymentStatusMail(mailData);
-            res.send(Payment)
+            let Payment =  paymentStatusMail(mailData);
+            console.log("Payment",Payment);
             let  dbName = await createClientDatabase(companycodevalue);
             if(dbName.status){
                 let insertClientMasterData=  await InsertClientMasterData(dbName.data)
@@ -2091,17 +2091,6 @@ async function setSprypleClientPlanPayment(req, res) {
                
            }
         });
-
-        //        let Payment = await paymentStatusMail(mailData);
-        //        console.log("Payment",Payment)
-        //        res.send(Payment)
-        //     let  dbName = await createClientDatabase(companycodevalue);
-        //     if(dbName.status){
-        //         let insertClientMasterData=  await InsertClientMasterData(dbName.data)
-        //     if(insertClientMasterData){
-        //         let datacreateClientCredentials =await createClientCredentials(companycodevalue);
-        //     }
-        // }
     
 
     
@@ -2381,8 +2370,8 @@ function createClientDatabase(companyName){
     return new Promise(async (res,rej)=>{
     const file_path = "./DB_Script/database_script.sql";
     var connection=mysql.createConnection({
-        host:"192.168.1.10",
-        // host:"122.175.62.210",
+        // host:"192.168.1.10",
+        host:"122.175.62.210",
         user:"spryple_product_user",
         password:"Spryple$#123",
         port: 3306,
@@ -2392,8 +2381,8 @@ function createClientDatabase(companyName){
     });
      connection.connect( function(err) {
         if (err) throw err;
-                const dbHost = "192.168.1.10";
-                // const dbHost ="122.175.62.210";
+                // const dbHost = "192.168.1.10";
+                const dbHost ="122.175.62.210";
                 const dbUser = "spryple_product_user";
                 const dbPassword = "Spryple$#123";
                 // Path to the MySQL dump file
@@ -2450,8 +2439,10 @@ function createClientDatabase(companyName){
                     if (err ) {
                         res(false);
                     } else {
-                        res(results[0][0]);
-                        sendEmailToSuperAdminCredentials(results[0][0],companyName)
+                       // res(results[0][0]);
+                       console.log(results[0][0]);
+                       console.log(results[1][0]);
+                        sendEmailToSuperAdminCredentials(results[1][0],companyName)
                     }
                 })
             }
@@ -2500,23 +2491,23 @@ function paymentStatusMail(mailData){
             html: html
         };
          transporter.sendMail(mailOptions, function (error, info) {
-            console.log("error",error);
             if (error) {
 
                 res(false);
             } else {
-                res({status:true});
+                res(true);
             }
         });
     });
 
 }
     function sendEmailToSuperAdminCredentials(mailData,companycodde) {
+        console.log("mailData",mailData)
         try {
            let email = mailData.login;
            let password = mailData.password_string;
            let companycode = companycodde;
-           let url= 'http://localhost:4500/#/Login'
+           let url= 'http://localhost:4200/#/Login'
            var transporter = nodemailer.createTransport({
            host: "smtp-mail.outlook.com", // hostname
            secureConnection: false, // TLS requires secureConnection to be false
