10777aef02aaba0b095dc0270e19576987434d46
bknasam <bknasam@sreebtech.com>
2023-05-10 17:42:13 +0530

    update the code in server.js

--- server.js
+++ server.js
@@ -3097,7 +3097,7 @@ app.listen(6060,function (err) {
     if (err)
         console.log('Server Cant Start ...Erorr....');
     else
-        console.log('Server Started at : http://localhost:7676');
+        console.log('Server Started at : http://localhost:6060');
 });
 
 /** uncomment in QA build time */
